8b004d081ea7bb4c295b4b9fd5b1ed6c9ef9c3cb
Arnold Mosha <arnoldmosha60@gmail.com>
2024-06-15 19:20:54 +0300

    progress

--- lib/auth/baseUrl.dart
+++ lib/auth/baseUrl.dart
@@ -1,3 +1,9 @@
 // ignore: file_names
+// for localhost
 // const baseURL = 'http://127.0.0.1:8000/';
-const baseURL = 'http://192.168.234.245:8000/';
+
+// using device ip
+// const baseURL = 'http://192.168.234.245:8000/';
+
+// using ngrok
+const baseURL = 'https://4047-41-222-180-110.ngrok-free.app/';
